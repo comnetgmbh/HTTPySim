d11ffa69dea008f6d505df7bddc037a8e6ad319a
Rika Lena Denia <rika.denia@comnetgmbh.com>
2018-09-03 15:18:01 +0200

    License and copyright update
    Let's make sure we're complying with FSF's recommended practices.

--- httpsim.py
+++ httpsim.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+#
+# Copyright 2018 Fabian Binder comNET GmbH <fabian.binder@comnetgmbh.com>
+#
+# This file is part of HTTPySim.
+#
+# HTTPySim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HTTPySim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HTTPySim.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from urlparse import urlparse
 import StringIO, argparse, os, sys, urllib2, gzip, io, yaml, time, re
